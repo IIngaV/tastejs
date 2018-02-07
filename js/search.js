1acb2b96439d993f6259ba89abee5693f898f5bf
IIngaV <inga1996@tut.by>
2018-02-07 17:41:19 +0300

    request

--- js/search.js
+++ js/search.js
@@ -1,32 +1,32 @@
 searchBtn.addEventListener( "click" , function() {
-	/*var xhr = new XMLHttpRequest();
+	var xhr = new XMLHttpRequest();
 	var url = 'https://api.nestoria.co.uk/api?encoding=json&action=search_listings&place_name='+city;
-	xhr.open('GET', url, true, null, null);
-	xhr.withCredentials = true;
+	xhr.open('GET', url, true);
 	xhr.send();
 	xhr.onreadystatechange = function(){
 		if (xhr.readyState != 4) {
     	return
     	}
 	}
-	alert(xhr.status);
+	alert("status: "+xhr.status);
 	if (xhr.status === 200) {
 	    console.log('result', xhr.responseText)
 	  } else {
 	    console.log('err', xhr.responseText)
 	  }
-	alert(xhr.responseText);*/
-	var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
+	alert(xhr.responseText);
+	/*var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
 	var xhr = new XHR();
 	xhr.open('GET', 'https://api.nestoria.co.uk/api?encoding=json&action=search_listings&place_name='+city, true);
+	xhr.send();
 	xhr.onload = function() {
-	  alert( this.responseText );
+	  console.log( this.responseText );
 	}
 	xhr.onerror = function() {
-	  alert( 'Ошибка ' + this.status );
+	  console.log( 'Ошибка ' + this.status );
 	}
-	alert(xhr.status);
-	xhr.send();
+	alert("status: "+xhr.status);*/
+	
 
 });
 
